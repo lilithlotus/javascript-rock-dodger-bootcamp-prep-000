e7294c3b20b83b340f9f6a20117bf1b60aecad64
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-23 02:34:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function endGame() {
   clearInterval(gameInterval);
   ROCKS.forEach(function(rock) { rock.remove() });
 
-  document.removeEventListener('keydown', moveDodger());
+  document.removeEventListener('keydown', moveDodger);
 
   return alert('YOU LOSE!')
 }
