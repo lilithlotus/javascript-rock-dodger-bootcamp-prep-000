4a33a0465572602ab936aba9244d6aa27704791f
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-23 02:23:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -126,6 +126,13 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
+  window.requestAnimationFrame(function() {
+    const left = positionToInteger(DODGER.style.left)
+
+    if (left > 0) {
+      DODGER.style.left = `${left - 4}px`;
+    }
+  });
   // implement me!
   /**
    * This function should move DODGER to the left
