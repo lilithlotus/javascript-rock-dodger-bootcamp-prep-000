0832590a2ba765c59f0983fd138ac1dfb8a9742e
Jessica Bensch <jessica.bensch11@gmail.com>
2017-09-23 02:30:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -141,18 +141,13 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  window.requestAnimationFrame(function() {
-    const  = positionToInteger(DODGER.style.left)
+  window.requestAnimationFrame(function () {
+    const left = positionToInteger(DODGER.style.left)
 
     if(left < 360) {
-      DODGER.syle.left = `${left + 4}px`
+      DODGER.style.left = `${left + 4}px`
     }
-  });
-  // implement me
-  /**
-   * This function should move DODGER to the right
-   * (mabye 4 pixels?). Use window.requestAnimationFrame()!
-   */
+  })
 }
 
 /**
